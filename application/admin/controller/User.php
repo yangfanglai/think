35de316b0454927dc25745ae1bd275d54158331e
yangfanglai <387807040@qq.com>
2018-04-25 23:51:58 +0800

    c

---
+++ application/admin/controller/User.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace app\admin\controller;
+
+class User
+{
+    public function index()
+    {
+        return '123';
+    }
+}
---
+++ application/index/controller/Log.php
@@ -0,0 +1,10 @@
+<?php
+namespace app\index\controller;
+
+class Log
+{
+    public function index()
+    {
+        echo '456';
+    }
+}
